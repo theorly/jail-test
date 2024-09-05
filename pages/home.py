e673aa87b149f5104b29a7a278a5c028c01a2e92
theorly <79010526+theorly@users.noreply.github.com>
2024-09-05 18:00:37 +0200

    info

--- pages/home.py
+++ pages/home.py
@@ -1,5 +1,4 @@
 import streamlit as st 
-from streamlit_card import card 
 from streamlit_option_menu import option_menu
 import streamlit as st
 import pandas as pd
@@ -8,12 +7,6 @@ import numpy as np
 
 st.subheader("Home Page")
 
-
-hasClicked = card(
-    title = "Click me",
-    text = "Click me to see the magic", 
-    image = "https://media.giphy.com/media/3o7TKz9bX9v6hZ8NSA/giphy.gif"
-)
 
 selected2 = option_menu(None, ['Option 1', 'Option 2', 'Option 3'], icons = ['🍎', '🍊', '🍇'],menu_icon="cast", default_index = 0, orientation = "horizontal")
 
